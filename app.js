e36345569cd086b5c0ebeb969244e43277d74dd7
Poli03 <99154416+Poli03@users.noreply.github.com>
2022-04-24 14:25:00 -0500

    se agrego carpeta db para persistencia de datos se agregaron funciones de leer y escribir archoivo data

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 require('colors');
 
 const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const { readDB, saveDB } = require('./helpers/saveFile');
 const Tasks = require('./models/tasks');
 
 const main = async() => {
     let opt='';
     const tasks = new Tasks();
+    const tasksDB = readDB();
+
+    if(tasksDB){
+      tasks.loadTaskFromArray(tasksDB);
+    }
 
     do{ 
       opt= await inquirerMenu();//imprime el menu
 
       switch (opt) {
-        case '1':
+        case '1'://crear tarea
           const desc = await readInput('Descripciòn:')
           tasks.createtask(desc);
         break;
@@ -20,7 +26,7 @@ const main = async() => {
           console.log(tasks.listArr)
         break;
       }
-
+      saveDB(tasks.listArr);
       await pause();
     }while(opt !== '7');
 }
--- models/tasks.js
+++ models/tasks.js
@@ -2,24 +2,30 @@ const Task = require("./task");
 
 
 class Tasks{
-    _listado={};
+    _list={};//pude ir o no solo se usa para ver clases
 
     constructor(){
-        this._listado={};
+        this._list={};
     }
      
     get listArr(){
         const list =[];
-        Object.keys(this._listado).forEach( key => {
-            const task = this._listado[key];
+        Object.keys(this._list).forEach( key => {
+            const task = this._list[key];
             list.push(task);
         });
         return list;
     }
 
+    loadTaskFromArray( tasks = [] ){
+        tasks.forEach( task => {
+            this._list[task.id] = task;
+        });
+    }
+
     createtask( desc = ''){
         const task = new Task(desc);
-        this._listado[task.id] = task
+        this._list[task.id] = task
     }
 }
 
